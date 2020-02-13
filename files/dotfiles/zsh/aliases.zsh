a829b92c9789d218a6b6df4d264b706efdd6348b
bashfulrobot <dustin@bashfulrobot.com>
2020-02-13 11:25:41 -0800

    Added vpn aliases for my feeble brain.

--- files/dotfiles/zsh/aliases.zsh
+++ files/dotfiles/zsh/aliases.zsh
@@ -12,3 +12,13 @@ alias opermissions="stat -c '%A %a %n'"
 alias octperm="stat -c '%A %a %n'"
 
 alias s="gnome-control-center sound"
+
+alias vpn-login="/usr/bin/nordvpn login"
+alias vpn-logout="/usr/bin/nordvpn logout"
+alias vpn-up="/usr/bin/nordvpn connect"
+alias vpn-down="/usr/bin/nordvpn diconnect"
+alias vpn-countries="/usr/bin/nordvpn countries"
+alias vpn-cities="/usr/bin/nordvpn cities"
+alias vpn-settings="/usr/bin/nordvpn settings"
+alias vpn-status="/usr/bin/nordvpn status"
+alias vpn-account="/usr/bin/nordvpn account"
