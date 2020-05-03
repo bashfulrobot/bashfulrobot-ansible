b07548d96c8f9327b5dc9622b2e3423e929b00ff
bashfulrobot <dustin@bashfulrobot.com>
2020-05-02 22:24:12 -0700

    🔥 Remove redundant function

--- files/dotfiles/zsh/aliases.zsh
+++ files/dotfiles/zsh/aliases.zsh
@@ -4,7 +4,6 @@
 # script, in alphabetical order.
 
 alias pcp='rsync -aP'
-alias cfg-pull='ansible-pull -U https://github.com/bashfulrobot/bashfulrobot-ansible.git'
 
 alias e='code-insiders -r'
 alias e-rust='code-insiders -r --extensions-dir /home/dustin/Documents/devel/vscode/profiles/rust/extensions --user-data-dir /home/dustin/Documents/devel/vscode/profiles/rust/user-data'
